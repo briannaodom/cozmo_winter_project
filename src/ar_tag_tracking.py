05640bd0314e23fbd2e7f9b7740048e40b8a0a64
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-02-12 18:30:38 -0600

    playing with PoseStamped v PointStamped in ar_tag_track fcn

--- src/ar_tag_tracking.py
+++ src/ar_tag_tracking.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int32, Header
+from std_msgs.msg import Int32
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image, Range
 from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 
 
 def ar_tag_track(data):
-    for AlvarMarker in data.markers:
-        movement_type = AlvarMarker.id
+    p = PoseStamped()
+    for m in data.markers:
+        movement_type = m.id
         if movement_type == 0:
             return
         else:
             if movement_type == 3:
-                Header.frame_id = 'follow'
+                p.header.frame_id = 'follow'
 
             elif movement_type == 4:
-                Header.frame_id = 'stop'
+                p.header.frame_id = 'stop'
 
-        m.pos.x = AlvarMarker.PoseStamped.pose.point.x
-        m.pos.y = AlvarMarker.PoseStamped.pose.point.y
-        m.pos.z = AlvarMarker.PoseStamped.pose.point.z
+        p.pose.position.x = m.pose.pose.point.x
+        p.pose.position.y = m.pose.pose.point.y
+        p.pose.position.z = m.pose.pose.point.z
 
-        print m.pos.x, m.pos.y, m.pos.z
+        print p
 
-        ar_tag_pub.publish(m.pos.x, m.pos.y, m.pos.z)
+        ar_tag_pub.publish(p)
 
         return
 
 if __name__ == '__main__':
     try:
         rospy.init_node('ar_tag_tracking.py')
-
-        rospy.Subscriber("/webcam_driver/image_raw", Image, )
         rospy.Subscriber("/ar_pose_marker", AlvarMarkers, ar_tag_track)
 
-        ar_tag_pub = rospy.Publisher('tag_position', PointStamped, queue_size=1)
+        ar_tag_pub = rospy.Publisher('tag_position', PoseStamped, queue_size=1)
 
-        except rospy.ROSInterruptException:
-            pass
+    except rospy.ROSInterruptException:
+        pass
