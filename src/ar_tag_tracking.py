c523eea209ead875497cabd1bab0cc8f8ec429dc
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-02-13 00:02:20 -0600

    edited ar_tag_track fcn and publisher

--- src/ar_tag_tracking.py
+++ src/ar_tag_tracking.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Int32
-from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Image, Range
+from geometry_msgs.msg import PointStamped
 from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 
 
 def ar_tag_track(data):
-    p = PoseStamped()
+    p = PointStamped()
     for m in data.markers:
         movement_type = m.id
         if movement_type == 0:
@@ -19,9 +18,9 @@ def ar_tag_track(data):
             elif movement_type == 4:
                 p.header.frame_id = 'stop'
 
-        p.pose.position.x = m.pose.pose.point.x
-        p.pose.position.y = m.pose.pose.point.y
-        p.pose.position.z = m.pose.pose.point.z
+        p.position.x = m.pose.pose.point.x
+        p.position.y = m.pose.pose.point.y
+        p.position.z = m.pose.pose.point.z
 
         print p
 
@@ -31,10 +30,13 @@ def ar_tag_track(data):
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('ar_tag_tracking.py')
+        rospy.init_node('ar_tag_tracking')
+
         rospy.Subscriber("/ar_pose_marker", AlvarMarkers, ar_tag_track)
 
-        ar_tag_pub = rospy.Publisher('tag_position', PoseStamped, queue_size=1)
+        ar_tag_pub = rospy.Publisher('tag_position', PointStamped, queue_size=1)
+
+        rospy.spin()
 
     except rospy.ROSInterruptException:
         pass
